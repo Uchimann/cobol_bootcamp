75891c7d5297837beae2be22a1844a437f4c85aa
İsmail Çelebi <133144077+Uchimann@users.noreply.github.com>
2023-06-24 09:20:55 +0300

    Update descriptions

--- Odevler/Hafta_2/Z95626.CBL(DCALC).cbl
+++ Odevler/Hafta_2/Z95626.CBL(DCALC).cbl
@@ -2,6 +2,9 @@
        PROGRAM-ID. DCALC.
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
+      *--Fıle control kısmında cobol dosyasında kullanacağımız PRINT-LINE değişkenini oluşturup
+      *--bu değişkeni jcl dosyasında dizini belirlediğimiz PRTLINE değişkenine hizalıyoruz(bağlıyoruz)
+      * aynı islemleri DATE-REC için de yapıyoruz.
        FILE-CONTROL.
            SELECT PRINT-LINE ASSIGN TO PRTLINE
                              STATUS ST-PRINT-LINE.
@@ -9,6 +12,9 @@
                              STATUS ST-DATE-REC.
        DATA DIVISION.
        FILE SECTION.
+      *PRINT-LINE dosyamızı açıyoruz ve PRINT-REC adında değişken yapısı ismi belirleyip, alt elemanlarını tanımlıyoruz
+      *(burdaki sıralamanın okunacak dosyadaki verilerin sıralaması ile aynı olduguna dikkat etmenizi isterim)
+      *bu sekil olmasının sebebi verileri sırayla sabit uzunluktaki bir yere yazdıracagımız icin diye yorumladim
        FD  PRINT-LINE RECORDING MODE F.
          01  PRINT-REC.
            03 REC-ID-O          PIC X(4).
@@ -36,13 +42,21 @@
            05 REC-LDAY          PIC 9(08).
 
       *--------------------
+      *Main paragrafimiz H100-OPEN_FILES paragrafını bitene kadar calistiriyor.
+      *ardindan H200 paragrafimiz DATE-REC-EOF degeri, degerimize gelene kadar calisiyor
+      *h999 ile dosyaları kapatip programi sonlandiriyoruz.
        PROCEDURE DIVISION.
        0000-MAIN.
            PERFORM H100-OPEN-FILES
            PERFORM H200-READ-NEXT-RECORD UNTIL DATE-REC-EOF
            PERFORM H999-PROGRAM-EXIT.
        0000-END. EXIT.
-      *---- H100 programi ilk once  DATA-REC dosyasini aciyor
+      *---- H100 programi ilk once  DATA-REC dosyasini input olarak aciyor
+      *daha sonra PRINT-LINE degiskenini output olarak aciyor.
+      * eger dosya durumları 0 ve 97 değil ise  hata mesaji verip return code degiskenimize, file status degerini atiyoruz
+      *00 ve 97 durumunda dosya basarili sekilde acilmistir. 97 durumunda Vsam icin, dosyanin bir önceki aciliminda duzgun sekilde
+      *acilmadigi anlamina gelir.
+      *ayni islemleri diger dosya durumlarimiz icin yapiyoruz.
        H100-OPEN-FILES.
            OPEN INPUT  DATE-REC.
            OPEN OUTPUT PRINT-LINE.
@@ -63,19 +77,21 @@
            PERFORM H999-PROGRAM-EXIT
            END-IF.
        H100-END. EXIT.
-
+      *CALL-RECORD paragrafimiz calistiriliyor. CALL-RECORD'dan geri gelisinde dosyadan bir satir okuyoruz.
        H200-READ-NEXT-RECORD.
                PERFORM CALC-RECORD
                READ DATE-REC.
        H200-END. EXIT.
-      *
+      *suanki ve dogum tarihini integere donusturup bir degiskene atiyoruz. Ardindan
+      *Write record paragrafimizi calistiriyoruz.
        CALC-RECORD.
            COMPUTE REC-DATE-INT = FUNCTION INTEGER-OF-DATE(REC-DATE)
            COMPUTE REC-NDATE-INT = FUNCTION INTEGER-OF-DATE(REC-NDATE)
            COMPUTE REC-LDAY = REC-NDATE-INT - REC-DATE-INT
            PERFORM WRITE-RECORD.
        CALC-END. EXIT.
-
+      *Bu paragraf, okunan degiskenleri, karsilik gelen yazdirilacak dosyamizda kullanacagimiz degiskenlere atiyor.
+      *Sonrasinda PRINT-REC degisken yapisini yazdiriyoruz. (ordaki alt degiskenler sirasiyla yazdiriliyor)
        WRITE-RECORD.
            MOVE REC-ID       TO  REC-ID-O.
            MOVE REC-NAME     TO  REC-NAME-O.
@@ -85,7 +101,7 @@
            MOVE REC-LDAY     TO  REC-LDAY-O.
            WRITE PRINT-REC.
        WRITE-END. EXIT.
-
+      *dosyalari kapatip programi durduruyoruz.
        H999-PROGRAM-EXIT.
            CLOSE DATE-REC.
            CLOSE PRINT-LINE.
